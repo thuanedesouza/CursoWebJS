6cbfb9897de6915e8d7ee3d7504d75e675b54f36
thuanedesouza <36061136+thuanedesouza@users.noreply.github.com>
2020-05-25 01:02:41 -0300

    atribuições

---
+++ exercicios-js/fundamentos/atribuicoes.js
@@ -0,0 +1,13 @@
+const a = 7
+let b = 3
+
+b += a// atribuição aditiva
+console.log(b)
+b -= a // atribuição subtrativa
+console.log(b)
+b *= a// atribuição multiplicativa
+console.log(b)
+b /= a// atribuição divisiva
+console.log(b)
+b %= 2// atribuição modular, mod sendo resto da divisão 
+console.log(b)
